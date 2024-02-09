34e464d5a049fab946dabe130706ea4004abfcde
KayodeAwe <beekay4laif@yahoo.com>
2024-02-09 14:37:49 +0100

    pre element use

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss'
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit{
 
+  Movie:any;
+  
+  constructor( private http:HttpClient){}
+
+  ngOnInit(): void {
+    this.getPopularMovies()
+  }
+
+  getPopularMovies = () => {
+    this.http.get(
+      'https://api.themoviedb.org/3/movie/popular?api_key=d44259f5724e5664c42d195042e2adf7'
+      ).subscribe((data) => {
+        this.Movie = data
+        console.log('The popular Movie', this.Movie.results)
+      })
+  }
 }
